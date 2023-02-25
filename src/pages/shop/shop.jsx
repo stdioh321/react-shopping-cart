bb34bee52ac1d4f0e304810b87c4a0f8146a0b54
Henrique <henrique.dias@gruponewway.com.br>
2023-02-25 19:44:41 -0300

    feature: adding search bar

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,27 +1,51 @@
-import React,{ useEffect } from 'react'
+import React,{ useEffect,useState } from 'react'
+import { debounce } from 'lodash'
 import Product from '../../components/product'
 import { PRODUCTS } from '../../utlis/products'
 import './shop.css'
 
 export default function Shop() {
-
+  const [products,setProducts] = useState(PRODUCTS)
   useEffect(() => {
-  },[])
+  },[products])
+
+  function handleSearch(query) {
+    console.log({ query });
+    let currQuery = query.toLowerCase().trim()
+    if (!currQuery || currQuery.length < 3) return setProducts(PRODUCTS)
+    const newProducts = [...PRODUCTS].filter((it,_) => {
+      const productName = it.productName.toLowerCase().trim()
+      return productName.match(currQuery)
+    })
+    setProducts(newProducts)
+  }
+  const handleSearchDebounce = debounce((e) => {
+    handleSearch(e.target.value)
+  },500)
   return (
     <div className='shop'>
       <div className='shopTitle'>
         <h1>My Shop</h1>
       </div>
-
+      <div className='searchBar'>
+        <input type="text" placeholder='Type your product....' onInput={handleSearchDebounce} />
+      </div>
       <div className="products">
         {
-          PRODUCTS.map((prod,idx) => {
+          products.map((prod,idx) => {
             return (<div key={idx}>
               <Product data={prod} />
             </div>)
           })
         }
       </div>
+      {
+        products.length < 1 && (
+          <div>
+            <h1 style={{ textAlign: 'center' }}>Not Found</h1>
+          </div>
+        )
+      }
     </div>
   )
 }
